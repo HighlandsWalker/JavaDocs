7449faaa33c4b7037163e8903dbeaedbaad67ae4
gabriel.tabus <ion.gabriel.tabus@gmail.com>
2017-07-06 09:22:25 +0300

    Java8 First Commit

---
+++ Collections/warmup-part1/src/main/java/exercise/exercise4/exceptions/MyIndexOutOfBoundsException.java
@@ -0,0 +1,10 @@
+package exercise.exercise4.exceptions;
+
+/**
+ * Created by Gabriel.Tabus on 7/5/2017.
+ */
+public class MyIndexOutOfBoundsException extends IndexOutOfBoundsException {
+    public MyIndexOutOfBoundsException(String mesaj){
+        super(mesaj);
+    }
+}
